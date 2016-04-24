130bf95396f6e806dc6e23c495fc0f95a0095260
xavier219 <fxt219@gmail.com>
2016-04-24 18:41:38 -0400

    Data to file test

--- test/JSON-YAML.py
+++ test/JSON-YAML.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
 
 import pprint
-test = 'Hello'
-pprint.pprint(test)
+import sys
+import yaml
+import json
+
+List1 = []
+
+for item in range(3):
+	List1.append(item)
+
+dic1 = {'test':range(5), 'booger':['snot', 'nose']}
+List1.append(dic1)
+
+with open('test.yml', 'w') as file:
+	file.write(yaml.dump(List1, default_flow_style=True))
+
+with open('test.json', 'w') as file:
+	json.dump(List1, file)
